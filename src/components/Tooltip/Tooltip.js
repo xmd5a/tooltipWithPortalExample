edd58ccc384e111effc74e6d944d04e688fd96b3
xmd5a <piotr.szarmach@uxpin.com>
2018-11-25 10:40:47 +0100

    removed unused constructor

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -2,10 +2,6 @@ import React, { PureComponent } from 'react';
 import Portal from '../Portal/Portal';
 
 class Tooltip extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return this.props.isVisible && <Portal>
       <span className="ds-tooltip">{this.props.children}</span>
